c25cf332f28bfad28b510d44c211ad22d1677d7c
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-24 17:06:25 +0100

    trying to stop duplicate email sign-ups, not working yet

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -23,13 +23,20 @@ feature 'User sign-up' do
     user = create :user, password_confirmation: 'wrong'
     expect { sign_up_as(user) }.not_to change(User, :count)
     expect(current_path).to eq '/users'
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario 'user can\'t sign up without entering an email' do
     user = create :user, email: nil
     expect { sign_up_as(user) }.not_to change(User, :count)
-
   end
+
+  scenario 'I cannot sign up with an existing email' do
+    user = create :user
+    sign_up_as(user)
+    expect{sign_up_as(user)}.to change(User, :count).by(0)
+    expect(page).to have_content 'Email is already taken'
+  end
+
 
 end
