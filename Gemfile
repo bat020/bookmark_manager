b97ba4f1c1e6c9fb24b1e353db00f239b3445158
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-24 12:17:56 +0100

    started implementing user ids, accounts etc

--- Gemfile
+++ Gemfile
@@ -1,16 +1,14 @@
-# A sample Gemfile
 source "https://rubygems.org"
-
-# gem "rails"
 
 ruby '2.2.3'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
-gem 'database_cleaner'
+gem 'bcrypt'
 
 group :test do
-gem 'rspec'
-gem 'capybara'
+  gem 'rspec'
+  gem 'capybara'
+  gem 'database_cleaner'
 end
--- app/app.rb
+++ app/app.rb
@@ -2,12 +2,16 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup.rb'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     'Hello BookmarkManager!'
   end
 
   get '/links' do
-  	@links = Link.all
+    @links = Link.all
   	erb :'links/index'
   end
 
@@ -22,13 +26,29 @@ class BookmarkManager < Sinatra::Base
       link.tags << Tag.first_or_create(name: name)
     end
     link.save
-    redirect ('/links')
+    redirect '/links'
   end
 
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
   	@links = tag ? tag.links : []
   	erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/links'
+  end
+
+  helpers do
+    def current_user
+      User.get(session[:user_id])
+    end
   end
 
   # start the server if ruby file executed directly
