176027fcc900f7a6586b52c77db33149f3adbe30
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-24 15:46:39 +0100

    flash messages + UI reconfiguration

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.2.3'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
+gem 'sinatra-flash'
 gem 'bcrypt'
 
 group :test do
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup.rb'
 
 class BookmarkManager < Sinatra::Base
 
   enable :sessions
+  register Sinatra::Flash
   set :session_secret, 'super secret'
 
   get '/' do
@@ -36,15 +38,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email:    params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/links'
+    @user = User.new(email:    params[:email],
+                     password: params[:password],
+                     password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/links'
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
